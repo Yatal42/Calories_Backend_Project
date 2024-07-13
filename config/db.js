e7abe7b5a92e60cfe687f87b1df22b48f95456d1
טל יעקובי <tal@h-MacBook-Pro-sl-tl.local>
2024-07-13 19:26:03 +0300

    zoom with gil

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/calories_manager?retryWrites=true&w=majority'
+    url: 'mongodb+srv://taltal2345:Tal315788653@cluster0.yudof5z.mongodb.net/caloriesDB'
     // Replace <username> and <password> with your MongoDB Atlas credentials
 };
